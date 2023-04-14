608f7d33a408158aa476bc1c99d811b6dc51dedc
Alex Panasko <a.panasko@bk.ru>
2023-04-14 21:46:24 +0500

    Fixed lombok

--- src/main/java/ru/netology/services/Radio.java
+++ src/main/java/ru/netology/services/Radio.java
@@ -2,26 +2,33 @@ package ru.netology.services;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 
 
 public class Radio {
 
+    private int maxStation = 9;
+    private int minStation = 0;
     private int currentStation;
+    private int maxVolume = 100;
+    private int minVolume = 0;
     private int currentVolume;
-    private int maxStation;
 
-    public Radio() {
-        this.maxStation = 9;
-    }
 
-    public Radio(int maxStation) {
-        this.maxStation = maxStation;
-    }
 
+    //
+//    public Radio() {
+//        this.maxStation = 9;
+//    }
+//
+//    public Radio(int maxStation) {
+//        this.maxStation = maxStation;
+//    }
+//
     public void nextStation() {
         if (currentStation >= maxStation) {
             currentStation = 0;
@@ -37,21 +44,21 @@ public class Radio {
             currentStation--;
         }
     }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
-            return;
-        }
-        if (currentStation > maxStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
-
+//
+//    public int getCurrentStation() {
+//        return currentStation;
+//    }
+//
+//    public void setCurrentStation(int currentStation) {
+//        if (currentStation < 0) {
+//            return;
+//        }
+//        if (currentStation > maxStation) {
+//            return;
+//        }
+//        this.currentStation = currentStation;
+//    }
+//
     public void increaseVolume() {
         if (currentVolume < 100) {
             currentVolume++;
@@ -63,18 +70,18 @@ public class Radio {
             currentVolume--;
         }
     }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            currentVolume = 0;
-        }
-        if (currentVolume > 100) {
-            currentVolume = 100;
-        }
-        this.currentVolume = currentVolume;
-    }
+//
+//    public int getCurrentVolume() {
+//        return currentVolume;
+//    }
+//
+//    public void setCurrentVolume(int currentVolume) {
+//        if (currentVolume < 0) {
+//            currentVolume = 0;
+//        }
+//        if (currentVolume > 100) {
+//            currentVolume = 100;
+//        }
+//        this.currentVolume = currentVolume;
+//    }
 }
